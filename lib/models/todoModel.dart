9f41909433c04f2317cc64c1e119dbdaf28e7ecd
Mateus Palmeira <jmateusps16@gmail.com>
2022-11-01 20:34:10 -0300

    Inicio

---
+++ lib/models/todoModel.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+class TodoModel {
+  final String titulo;
+  final String subtitulo;
+  final bool finalizado;
+  TodoModel({
+    required this.titulo,
+    required this.subtitulo,
+    required this.finalizado,
+  });
+
+  TodoModel copyWith({
+    String? titulo,
+    String? subtitulo,
+    bool? finalizado,
+  }) {
+    return TodoModel(
+      titulo: titulo ?? this.titulo,
+      subtitulo: subtitulo ?? this.subtitulo,
+      finalizado: finalizado ?? this.finalizado,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'titulo': titulo,
+      'subtitulo': subtitulo,
+      'finalizado': finalizado,
+    };
+  }
+
+  factory TodoModel.fromMap(Map<String, dynamic> map) {
+    return TodoModel(
+      titulo: map['titulo'] as String,
+      subtitulo: map['subtitulo'] as String,
+      finalizado: map['finalizado'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory TodoModel.fromJson(String source) =>
+      TodoModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() =>
+      'TodoModel(titulo: $titulo, subtitulo: $subtitulo, finalizado: $finalizado)';
+
+  @override
+  bool operator ==(covariant TodoModel other) {
+    if (identical(this, other)) return true;
+
+    return other.titulo == titulo &&
+        other.subtitulo == subtitulo &&
+        other.finalizado == finalizado;
+  }
+
+  @override
+  int get hashCode =>
+      titulo.hashCode ^ subtitulo.hashCode ^ finalizado.hashCode;
+}
